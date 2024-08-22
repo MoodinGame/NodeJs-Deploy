64e46ba9391d2afabe452aba70c89815bc9f099e
Wanchana Chanyaemsong <C.wanchana1998@gmail.com>
2024-08-22 23:48:52 +0700

    Add all file of Project
    Hello world

---
+++ app.mjs
@@ -0,0 +1,15 @@
+import express from "express";
+import chalk from "chalk";
+import debug from "debug";
+import morgan from "morgan";
+const app = express();
+const port = 3000;
+app.use("combined");
+
+app.get("/", (req, res) => {
+  res.send("Hello Game");
+});
+
+app.listen(port, () => {
+  debug("Listening on port " + chalk.green(port));
+});
