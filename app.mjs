b4c4be9ca948d4581b45d69e8edd1b535a0b65cc
Wanchana Chanyaemsong <C.wanchana1998@gmail.com>
2024-08-23 00:54:02 +0700

    update

--- app.mjs
+++ app.mjs
@@ -2,9 +2,17 @@ import express from "express";
 import chalk from "chalk";
 import debug from "debug";
 import morgan from "morgan";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url"
+
 const app = express();
 const port = 3000;
-app.use("combined");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/Pubilc/")));
 
 app.get("/", (req, res) => {
   res.send("Hello Game5555");
